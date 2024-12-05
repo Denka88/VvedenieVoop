e43c0d5fd617a4a4abf8fb5ecde68e4c03b116bf
Denka88 <shadowfightdenka88@gmail.com>
2024-12-05 17:33:15 +0300

    Classes created

---
+++ src/Product.java
@@ -0,0 +1,49 @@
+public class Product {
+
+    String name;
+    String description;
+    float price;
+    int quantity;
+
+    public Product() {
+    }
+
+    public Product(String name, String description, float price, int quantity) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
